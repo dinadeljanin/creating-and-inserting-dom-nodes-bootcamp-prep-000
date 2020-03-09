7a0f824136aa892bd52d4202f14c522f13e4317c
D D <ddeljanin5@icloud.com>
2020-03-09 19:22:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 // adding js anyways
 
+// awesome it works, but it doesn't show up yet
 var element = document.createElement('div');
+element.innerHTML = 'Hello, DOM!';
+element.style.backgroundColor = '#f9f9f9';
+
